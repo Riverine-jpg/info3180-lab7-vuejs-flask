1e59518a8d6094576db84afc6949d7f4c1e6e787
Riverine-jpg <nateb2001@gmail.com>
2022-04-10 22:57:57 -0500

    css for bonus question added

--- src/components/UploadForm.vue
+++ src/components/UploadForm.vue
@@ -1,16 +1,16 @@
 <template>
     <div class = "container">
-        <div v-if="response.errors != 'novalue'">
+        <div v-if="response.errors != 'novalue'" class = "error-message">
             <ul id="vlist">
                 <li v-for="(error, index) in response.errors">
                     {{ error }}
                 </li>
             </ul>
         </div>
-        <div v-if="response.message != 'novalue'">
+        <div v-if="response.message != 'novalue'" class = "success-message">
             {{response.message}}
         </div>
-        <form method="POST" enctype="multipart/form-data" id = "uploadForm" @submit.prevent="uploadPhoto" >
+        <form method="POST" enctype="multipart/form-data" id = "uploadForm" @submit.prevent="uploadPhoto" class = "formc">
             <ul class="formstf">
                 <li class="form-field">
                     <label>Description <span class="required">*</span></label>
@@ -22,7 +22,7 @@
                     <input type="file" accept="image/*" class="form-control-file" @change="updatePhoto($event.target.files)" id ="photo" name= "photo">
                 </li>
             </ul>
-            <input id="submit" type="submit" value="submit" @click.prevent="uploadPhoto()"/>
+            <input class = "button" id="submit" type="submit" value="submit" @click.prevent="uploadPhoto()"/>
         </form> 
     </div>
 </template>
@@ -109,5 +109,64 @@ export default {
 </script>
 
 <style>
+ .formc{
+    margin:10px auto;
+    max-width: 400px;
+    padding: 20px 12px 10px 20px;
+    font: 13px "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+  }
+  .formc li {
+    padding: 0;
+    display: block;
+    list-style: none;
+    margin: 10px 0 0 0;
+  }
+  .formc label{
+    margin:0 0 3px 0;
+    padding:0px;
+    display:block;
+    font-weight: bold;
+  }
+  
+  .formc input,textarea{
+      box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    border:1px solid #BEBEBE;
+    padding: 7px;
+    margin:0px;
+    outline: none;
+      width: 100%;
+  }
+  
+  .formc textarea{
+    height: 100px;
+      width: 100%;
+  }
+  
+  .formc .required{
+    color:red;
+  }
+  
+  .formc label{
+      width:80%;
+  }
+  
+ .formc .button{
+      background: #2f4ad0;
+      border: none;
+      padding: 8px 15px 8px 15px;
+    border: none;
+      color:white;
+  }
 
+  .error-message{
+      background-color: #F8D7DA;
+      color: #701A22;
+  }
+
+  .success-message{
+      background-color:#D4EDDA;
+      color: #3D7553;
+  }
 </style>
